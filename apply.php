eb88d407d590268f18a993b8b52dd7453d896a6f
Vansh Sukhija <105533624@student.swin.edu.au>
2026-06-01 20:07:29 +1000

    Enhance accessibility of job application form
    Added ARIA attributes and improved accessibility for form elements.

--- apply.php
+++ apply.php
@@ -19,7 +19,7 @@ require_once(__DIR__ . "/inc/header.inc");
                 Please fill in your details below to apply. All fields are required.
             </p>
             
-            <form class="form-container" method="post" action="process_eoi.php" novalidate>    
+            <form class="form-container" method="post" action="process_eoi.php" novalidate aria-label="Job Application Form">    
                 
                 <fieldset>
                     <legend><b>Job Application Details</b></legend>
@@ -29,24 +29,26 @@ require_once(__DIR__ . "/inc/header.inc");
                         type="text" 
                         id="jobref" 
                         name="jobref"
+                        aria-label="Job reference number"
+                        aria-required="true"
                         value="<?php echo $jobref_value; ?>" 
                         <?php echo !empty($jobref_value) ? 'readonly style="background-color: #e9ecef;"' : ''; ?> 
                     >
 
                     <label for="firstname">First name</label>
-                    <input type="text" id="firstname" name="firstname">
+                    <input type="text" id="firstname" name="firstname" aria-label="First name" aria-required="true">
 
                     <label for="lastname">Last name</label>
-                    <input type="text" id="lastname" name="lastname">
+                    <input type="text" id="lastname" name="lastname" aria-label="Last name" aria-required="true">
 
                     <label for="date">Date of birth</label>
-                    <input type="text" id="date" name="date" placeholder="DD/MM/YYYY">
+                    <input type="text" id="date" name="date" placeholder="DD/MM/YYYY" aria-label="Date of birth in DD/MM/YYYY format" aria-required="true">
                 </fieldset>
 
-                <fieldset>
-                    <legend><b>Gender</b></legend>
+                <fieldset role="group" aria-labelledby="gender-legend">
+                    <legend id="gender-legend"><b>Gender</b></legend>
 
-                    <input type="radio" id="male" name="gender" value="male">
+                    <input type="radio" id="male" name="gender" value="male" aria-required="true">
                     <label for="male">Male</label>
 
                     <input type="radio" id="female" name="gender" value="female">
@@ -60,13 +62,13 @@ require_once(__DIR__ . "/inc/header.inc");
                     <legend><b>Address</b></legend>
 
                     <label for="street">Street Address</label>
-                    <input type="text" id="street" name="street">
+                    <input type="text" id="street" name="street" aria-label="Street address" aria-required="true">
 
                     <label for="suburb">Suburb/Town</label>
-                    <input type="text" id="suburb" name="suburb">
+                    <input type="text" id="suburb" name="suburb" aria-label="Suburb or town" aria-required="true">
 
                     <label for="state">State</label>
-                    <select id="state" name="state">
+                    <select id="state" name="state" aria-label="State" aria-required="true">
                         <option value="" disabled selected>Please Select</option>
                         <option value="VIC">VIC</option>
                         <option value="NSW">NSW</option>
@@ -79,29 +81,29 @@ require_once(__DIR__ . "/inc/header.inc");
                     </select>
 
                     <label for="postcode">Postcode</label>
-                    <input type="text" id="postcode" name="postcode">
+                    <input type="text" id="postcode" name="postcode" aria-label="4 digit postcode" aria-required="true">
                 </fieldset>
 
                 <fieldset>
                     <legend><b>Contact Information</b></legend>
 
                     <label for="email">Email</label>
-                    <input type="text" id="email" name="email" placeholder="example@gmail.com">
+                    <input type="text" id="email" name="email" placeholder="example@gmail.com" aria-label="Email address" aria-required="true">
 
                     <label for="phone">Phone number</label>
-                    <input type="text" id="phone" name="phone">
+                    <input type="text" id="phone" name="phone" aria-label="Phone number between 8 and 12 digits" aria-required="true">
                 </fieldset>
 
-                <fieldset>
-                    <legend><b>Skills Needed</b></legend>
+                <fieldset role="group" aria-labelledby="skills-legend">
+                    <legend id="skills-legend"><b>Skills Needed</b></legend>
 
-                    <input type="checkbox" id="skill-html" name="skills[]" value="HTML">
+                    <input type="checkbox" id="skill-html" name="skills[]" value="HTML" aria-label="HTML skill">
                     <label for="skill-html">HTML</label>
 
-                    <input type="checkbox" id="skill-css" name="skills[]" value="CSS">
+                    <input type="checkbox" id="skill-css" name="skills[]" value="CSS" aria-label="CSS skill">
                     <label for="skill-css">CSS</label>
 
-                    <input type="checkbox" id="skill-js" name="skills[]" value="JavaScript">
+                    <input type="checkbox" id="skill-js" name="skills[]" value="JavaScript" aria-label="JavaScript skill">
                     <label for="skill-js">JavaScript</label>
                 </fieldset>
 
@@ -109,12 +111,14 @@ require_once(__DIR__ . "/inc/header.inc");
                     <legend><b>Other Skills</b></legend>
 
                     <label for="otherskills">List any additional skills you have:</label>
-                    <textarea id="otherskills" name="otherskills" rows="4" cols="50" placeholder="e.g. Figma, Adobe Illustrator, team leadership..."></textarea>
+                    <textarea id="otherskills" name="otherskills" rows="4" cols="50" 
+                        placeholder="e.g. Figma, Adobe Illustrator, team leadership..."
+                        aria-label="Other skills"></textarea>
                 </fieldset>
 
                 <div class="form-buttons">
-                    <button type="submit">Submit Application</button>
-                    <button type="reset">Reset Form</button>
+                    <button type="submit" aria-label="Submit application">Submit Application</button>
+                    <button type="reset" aria-label="Reset form">Reset Form</button>
                 </div>
 
             </form>
